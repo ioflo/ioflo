cfade5f37b31f2f039eafe6dead184ab5bb811c2
Samuel M Smith <smith.samuel.m@gmail.com>
2014-01-13 10:51:49 -0700

    fixed include

--- ioflo/trim/interior/fancy/__init__.py
+++ ioflo/trim/interior/fancy/__init__.py
@@ -3,10 +3,10 @@
 """
 #print "\nPackage at%s" % __path__[0]
 
-__all__ = ['cloning', 'salting']
+__all__ = ['cloning']
 
 for m in __all__:
     exec "from . import %s" % m  #relative import
 
 #used by CreateAllInstances                 
-_InstanceModules = [cloning, salting]
+_InstanceModules = [cloning]
