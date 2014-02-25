8cae4d6e8f36e70731ac227377de6d8a0a86c18e
Samuel M Smith <smith.samuel.m@gmail.com>
2014-02-25 13:02:13 -0700

    Added test of run.start

---
+++ ioflo/app/test/testStart.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Runs alls the example FloScripts
+
+
+"""
+import sys
+import os
+
+import ioflo.app.run
+
+PLAN_DIR_PATH = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    , 'plan')
+
+def getPlanFiles(planDirPath=PLAN_DIR_PATH):
+    planFiles = []
+    for fname in os.listdir(os.path.abspath(planDirPath)):
+        root, ext = os.path.splitext(fname)
+        if ext != '.flo' or root.startswith('__'):
+            continue
+
+        planFiles.append(os.path.abspath(os.path.join(planDirPath, fname)))
+    return planFiles
+
+def test():
+    """ Execute run.start """
+    plans = getPlanFiles()
+    filepath = "../plan/meta.flo"
+    opts = dict(gandolf='grey', saruman='white')
+    metadata = [("opts", ".testmeta.opts", dict(value=opts))]
+
+    ioflo.app.run.start(
+                        name='teststart',
+                        period=0.125,
+                        stamp=0.0,
+                        real=False,
+                        filepath=filepath,
+                        behaviors=None,
+                        username="",
+                        password="",
+                        mode=None,
+                        houses=None,
+                        metadata=metadata,
+                        verbose=2,
+                        )
+
+if __name__ == '__main__':
+    test()
+
+
