3d0cdd26e59cbecee985ba955ab088f23705c524
Samuel M Smith <smith.samuel.m@gmail.com>
2014-06-06 17:23:26 -0600

    removed comment

--- ioflo/__init__.py
+++ ioflo/__init__.py
@@ -12,10 +12,6 @@ __author__ = "Samuel M. Smith"
 __license__ =  "MIT"
 
 
-#from .base.consoling import getConsole
-#console = getConsole()
-#console.profuse("{0} version {1}\n".format(__path__[0], __version__))
-
 __all__ = ['base', 'trim']
 
 for m in __all__:
