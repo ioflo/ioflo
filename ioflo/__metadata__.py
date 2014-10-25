a369d48e9cc842acd53b15e1ff1e9b3eeb758899
C. R. Oldham <cro@ncbt.org>
2014-10-25 14:41:20 -0400

    Change to pattern used in raet for importing version information to avoid missing importlib on python26.

---
+++ ioflo/__metadata__.py
@@ -0,0 +1,11 @@
+'''
+Ioflo package metadata
+'''
+
+__version_info__ = (1, 0, 1)
+__version__ = '{0}.{1}.{2}'.format(*__version_info__)
+__author__ = "Samuel M. Smith"
+__license__ = "Apache 2.0"
+
+__all__ = ['__version_info__', '__version__', '__author__', '__license__']'
+
