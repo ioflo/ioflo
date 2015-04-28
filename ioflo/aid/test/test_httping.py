1cd1ffcea871d50ef9b3425ff81c09d4307e6ae5
Samuel M Smith <smith.samuel.m@gmail.com>
2015-04-28 12:44:32 -0600

    add space

--- ioflo/aid/test/test_httping.py
+++ ioflo/aid/test/test_httping.py
@@ -92,7 +92,7 @@ class BasicTestCase(unittest.TestCase):
         console.terse("{0}\n".format("Post ...\n"))
         headers = odict([('Accept', 'application/json'), ('Content-Type', 'application/json')])
         body = odict([('name', 'Peter'), ('occupation', 'Engineer')])
-        body = ns2b(json.dumps(body, separators=(',', ':'),encoding='utf-8'))
+        body = ns2b(json.dumps(body, separators=(',', ':'), encoding='utf-8'))
         hc.request(method='POST', url='/demo', body=body, headers=headers )
         response = hc.getresponse()
         console.terse(str(response.fileno()) + "\n") # must call this before read
