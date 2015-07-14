1076e18fe8920ce861aba14a836fd8a4d9c88aeb
Samuel M Smith <smith.samuel.m@gmail.com>
2015-07-14 17:13:13 -0600

    placeholder for backwards compat

---
+++ ioflo/base/aiding.py
@@ -0,0 +1,12 @@
+"""
+aiding.py
+
+"""
+#print("module {0}".format(__name__))
+
+from __future__ import division
+
+# Backwards compatibility for now
+# In future users should import from ioflo.aid.aiding not here
+from ..aid.aiding import *
+
