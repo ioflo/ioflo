a4eb32d94dc9aec6b22d114737c4885ad67c46f9
Samuel M Smith <smith.samuel.m@gmail.com>
2015-10-16 22:11:17 -0600

    removed duplicate definitions

--- ioflo/aid/http/serving.py
+++ ioflo/aid/http/serving.py
@@ -49,95 +49,6 @@ console = getConsole()
 CRLF = b"\r\n"
 LF = b"\n"
 CR = b"\r"
-MAX_LINE_SIZE = 65536
-MAX_HEADERS = 100
-
-HTTP_PORT = 80
-HTTPS_PORT = 443
-HTTP_11_VERSION_STRING = u'HTTP/1.1'  # http v1.1 version string
-
-# status codes
-# informational
-CONTINUE = 100
-
-# These constants are here for potential backwards compatibility
-# currently most are unused
-# status codes
-# informational
-
-SWITCHING_PROTOCOLS = 101
-PROCESSING = 102
-
-# successful
-OK = 200
-CREATED = 201
-ACCEPTED = 202
-NON_AUTHORITATIVE_INFORMATION = 203
-NO_CONTENT = 204
-RESET_CONTENT = 205
-PARTIAL_CONTENT = 206
-MULTI_STATUS = 207
-IM_USED = 226
-
-# redirection
-MULTIPLE_CHOICES = 300
-MOVED_PERMANENTLY = 301
-FOUND = 302
-SEE_OTHER = 303
-NOT_MODIFIED = 304
-USE_PROXY = 305
-TEMPORARY_REDIRECT = 307
-
-# client error
-BAD_REQUEST = 400
-UNAUTHORIZED = 401
-PAYMENT_REQUIRED = 402
-FORBIDDEN = 403
-NOT_FOUND = 404
-METHOD_NOT_ALLOWED = 405
-NOT_ACCEPTABLE = 406
-PROXY_AUTHENTICATION_REQUIRED = 407
-REQUEST_TIMEOUT = 408
-CONFLICT = 409
-GONE = 410
-LENGTH_REQUIRED = 411
-PRECONDITION_FAILED = 412
-REQUEST_ENTITY_TOO_LARGE = 413
-REQUEST_URI_TOO_LONG = 414
-UNSUPPORTED_MEDIA_TYPE = 415
-REQUESTED_RANGE_NOT_SATISFIABLE = 416
-EXPECTATION_FAILED = 417
-UNPROCESSABLE_ENTITY = 422
-LOCKED = 423
-FAILED_DEPENDENCY = 424
-UPGRADE_REQUIRED = 426
-PRECONDITION_REQUIRED = 428
-TOO_MANY_REQUESTS = 429
-REQUEST_HEADER_FIELDS_TOO_LARGE = 431
-
-# server error
-INTERNAL_SERVER_ERROR = 500
-NOT_IMPLEMENTED = 501
-BAD_GATEWAY = 502
-SERVICE_UNAVAILABLE = 503
-GATEWAY_TIMEOUT = 504
-HTTP_VERSION_NOT_SUPPORTED = 505
-INSUFFICIENT_STORAGE = 507
-NOT_EXTENDED = 510
-NETWORK_AUTHENTICATION_REQUIRED = 511
-
-
-
-METHODS = (u'GET', u'HEAD', u'PUT', u'PATCH', u'POST', u'DELETE',
-           u'OPTIONS', u'TRACE', u'CONNECT' )
-
-# maximal amount of data to read at one time in _safe_read
-MAXAMOUNT = 1048576
-
-# maximal line length when calling readline().
-_MAXLINE = 65536
-_MAXHEADERS = 100
-
 
 
 #  Class Definitions
@@ -376,7 +287,7 @@ class Responder(object):
     Date: Thu, 30 Apr 2015 19:37:17 GMT\r\n
     Server: IoBook.local\r\n\r\n
     """
-    HttpVersionString = HTTP_11_VERSION_STRING  # http version string
+    HttpVersionString = httping.HTTP_11_VERSION_STRING  # http version string
 
     def __init__(self,
                  steward=None,
@@ -756,7 +667,7 @@ class WsgiResponder(object):
     Date: Thu, 30 Apr 2015 19:37:17 GMT\r\n
     Server: IoBook.local\r\n\r\n
     """
-    HttpVersionString = HTTP_11_VERSION_STRING  # http version string
+    HttpVersionString = httping.HTTP_11_VERSION_STRING  # http version string
 
     def __init__(self,
                  incomer,
