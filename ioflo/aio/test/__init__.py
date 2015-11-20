ab1835cf4116a60e70d10aa04b8265b4d53a1da9
Samuel M Smith <smith.samuel.m@gmail.com>
2015-11-20 12:14:51 -0700

    moved io files to new package

---
+++ ioflo/aio/test/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+ioflo.aio asynchronous io  test package
+
+To run all the unittests:
+
+from ioflo import test
+test.run()
+
+"""
+import sys
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+import os
+
+from ioflo import test
+from ioflo.aid.consoling import getConsole
+console = getConsole()
+console.reinit(verbosity=console.Wordage.concise)
+
+top = os.path.dirname(os.path.abspath(sys.modules.get(__name__).__file__))
+
+if __name__ == "__main__":
+    test.run(top)
