8a659c546379cd6ac4b53ef4b483157f11c6de00
Samuel M Smith <smith.samuel.m@gmail.com>
2016-05-03 12:44:50 -0600

    updated doc string

--- ioflo/base/acting.py
+++ ioflo/base/acting.py
@@ -560,6 +560,12 @@ class Actor(object):
         where shares associated with the Actor instance may be placed when
         relative addressing is used.
 
+        The inode is computed as absolute, relative, or default.
+        When not provided the default inode is  'framer.me.frame.me.actor.me.'
+        When the inode is relative it is relative to the framer inode
+        If the framer is a cloned auxiliary then the inode could be relative
+          to the main framer inode
+
         The values of the items in the **kwa argument may be either strings or
         mappings
 
@@ -636,7 +642,7 @@ class Actor(object):
                 if finode == "main":  # replace finode with main framer inode
                     finode = framer.main.framer.inode
                 elif finode == "mine":  # don't prepend main framer inode
-                    finode = ""
+                    finode = ""  # effectively empty inode
                 else:  # possibly prepend main framer inode if finode relative
                     minode = framer.main.framer.inode
                     minode = minode.rstrip('.')
