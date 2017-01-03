b6183c3ee88fbb2476b51652161702ed63180acf
Samuel M Smith <smith.samuel.m@gmail.com>
2017-01-02 18:51:47 -0700

    added failfast parameter

--- ioflo/test/__init__.py
+++ ioflo/test/__init__.py
@@ -27,7 +27,7 @@ start = os.path.dirname(os.path.dirname
 # need top to be above root for relative imports to not go above top level
 top = os.path.dirname(start)
 
-def run(top, start=None):
+def run(top, start=None, failfast=False):
     """
     Run unittests starting at directory given by start within the package rooted at top
     """
@@ -37,7 +37,7 @@ def run(top, start=None):
     console.terse("\nRunning ioflo tests starting at '{0}' from '{1}', \n".format(start, top))
     loader = unittest.TestLoader()
     suite = loader.discover(start, 'test_*.py', top )
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.TextTestRunner(verbosity=2, failfast=failfast).run(suite)
 
 if __name__ == "__main__":
     run(top, start)
